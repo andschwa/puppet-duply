1f833f6c5bd0b9d08fda70cbe4c2f68d664cc754
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-01-22 00:34:59 -0800

    Adding duply homebrew formula at 1.6.0-boxen1

---
+++ files/brews/duply.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Duply < Formula
+  homepage 'http://duply.net'
+  url 'http://downloads.sourceforge.net/project/ftplicity/duply%20(simple%20duplicity)/1.6.x/duply_1.6.0.tgz'
+  sha1 '0e376c331b26b64cf1d537b3d9f417eac393fd92'
+  version '1.6.0-boxen1'
+
+  depends_on 'duplicity'
+
+  def install
+    bin.install 'duply'
+  end
+end
